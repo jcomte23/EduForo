1d7a6b3c5b90a55edca5d24a3fbfcc5ddb9e05d3
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-06-26 15:46:31 -0500

    mejoras en el sistemas de navegacion

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,7 +15,7 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.index','post.create','post.edit')">
+                    <x-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.*')">
                         {{ __('Posts') }}
                     </x-nav-link>
                 </div>
@@ -179,6 +179,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="{{ route('post.index') }}" :active="request()->routeIs('post.*')">
+                {{ __('Posts') }}
             </x-responsive-nav-link>
         </div>
 
